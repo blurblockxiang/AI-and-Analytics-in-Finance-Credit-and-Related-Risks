fc1d524560f022b7850feeb1cdd5c9bed371b28e
blurblockxiang <blurblockxiang@hotmail.com>
2024-09-28 11:44:12 +0800

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template, request
+import google.generativeai as genai
+import os
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+api = os.getenv("MAKERSUITE")
+genai.configure(api_key=api)
 
 app = Flask(__name__)
 
@@ -20,7 +26,22 @@ def prediction_result_DBS():
         return render_template("prediction_result_DBS.html", r=r)
     else:
         # Handle cases where q is None or invalid
-        return render_template("prediction_result_DBS.html", r="Invalid input")
+        return (render_template("prediction_result_DBS.html", r="Invalid input"))
+
+@app.route("/faq", methods=["GET", "POST"])
+def faq():
+    return render_template("faq.html")
+
+@app.route("/q1", methods=["GET", "POST"])
+def q1():
+    r = model.generate_content("How should i diversify my investment portfolio")
+    return render_template("q1_reply.html", r=r)
+
+@app.route("/q2", methods=["GET", "POST"])
+def q2():
+    q = request.form.get("q")
+    r = model.generate_content(q)
+    return render_template("q2_reply.html", r=r)
 
 if __name__ == "__main__":
     app.run(debug=True)
